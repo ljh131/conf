cb21b2dffdbdddcf51e5113d1b32cbd76e7c3e2e
ljh131 <ljh131@gmail.com>
2020-11-23 17:05:50 +0900

    add tmux3 conf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 ln -s $(pwd)/.mybashrc ~
-ln -s $(pwd)/.tmux.conf ~
+#ln -s $(pwd)/.tmux.conf ~
+ln -s $(pwd)/.tmux3.conf ~/.tmux.conf
 ln -s $(pwd)/.vimrc ~
 ln -s $(pwd)/.ideavimrc ~
 ln -s $(pwd)/.gitconfig ~
